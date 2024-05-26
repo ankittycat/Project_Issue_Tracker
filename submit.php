fe63b39c1e2a63a3367e459f25496e9eedde23f8
ankittycat <159798470+ankittycat@users.noreply.github.com>
2024-05-26 17:06:43 +0300

    submit.php

--- submit.php
+++ submit.php
@@ -4,17 +4,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $description = htmlspecialchars($_POST['description']);
     $priority = htmlspecialchars($_POST['priority']);
     
-    // Prepare the issue data
     $issue = "$title|$description|$priority\n";
     
-    // Save the issue to the file
     file_put_contents('issues.txt', $issue, FILE_APPEND);
     
-    // Redirect back to the main page
     header('Location: index.php');
     exit();
 } else {
-    // If accessed directly, redirect to the form
     header('Location: index.php');
     exit();
 }
